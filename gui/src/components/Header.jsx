88349451041d721f164357370290a9c2014e18b9
Samet Alkis <samet@alkis.tech>
2024-06-27 13:01:42 +0000

    header title removed

--- gui/src/components/Header.jsx
+++ gui/src/components/Header.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="w-full h-20 text-bgray-150 bg-black relative">
-      <h1 className="text-4xl font-bold absolute top-5 left-5 2xl:left-40 xl:left-16 lg:left-48 md:left-24">
-        YT-DLP GUI
-      </h1>
+      <h1 className="text-4xl font-bold absolute top-5 left-5 2xl:left-40 xl:left-16 lg:left-48 md:left-24"></h1>
     </div>
   );
 }
