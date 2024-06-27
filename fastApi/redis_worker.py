57851a79c8d5b52882fcbfa58db2a9cb648803f4
Samet Alkis <samet@alkis.tech>
2024-06-27 14:06:22 +0000

    unnecessary files have been deleted and made available for use by everyone

--- fastApi/redis_worker.py
+++ fastApi/redis_worker.py
@@ -8,5 +8,3 @@ REDIS_PASS = os.getenv('REDIS_PASS')
 
 redis_conn = Redis(host="redis", port="6379", password=REDIS_PASS)
 task_queue = Queue("task_queue", connection=redis_conn)
-
-
--- fastApi/start.sh
+++ fastApi/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
-rq worker-pool --url redis://:k7fkuXq9Kf8QDW88KiFE@redis:6379 task_queue -n 25
+rq worker-pool --url redis://:<REDIS_PASS>@redis:6379 task_queue -n 25
