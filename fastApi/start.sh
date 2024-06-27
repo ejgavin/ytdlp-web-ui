df6b9beaa1ee1a42cae1af0d91e4f54c8b1af0ad
Samet Alkis <samet@alkis.tech>
2024-06-27 18:25:30 +0000

    added default config

--- fastApi/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
-rq worker-pool --url redis://:<REDIS_PASS>@redis:6379 task_queue -n 25
--- gui/vite.config.js
+++ gui/vite.config.js
@@ -3,27 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://manifest.googlevideo.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        configure: (proxy, options) => {
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            proxyReq.setHeader("Origin", "https://manifest.googlevideo.com");
-          });
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-            proxyRes.headers["Access-Control-Allow-Methods"] =
-              "GET,PUT,POST,DELETE,PATCH,OPTIONS";
-            proxyRes.headers["Access-Control-Allow-Headers"] =
-              "Origin, X-Requested-With, Content-Type, Accept, Authorization";
-            proxyRes.headers["Sec-Fetch-Mode"] = "no-cors";
-          });
-        },
-      },
-    },
-  },
+  server: {},
 });
